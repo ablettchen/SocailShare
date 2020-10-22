f1d386be5ccc5def691c94f6c7cefa012921a0bb
ablett <ablettchen@gmail.com>
2020-10-22 16:34:34 +0800

    mod -

--- SocailShare.podspec
+++ SocailShare.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
     
   s.name             = 'SocailShare'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = '社会化分享'
   s.description      = <<-DESC
   TODO: Add long description of the pod here.
--- SocailShare/Classes/Extensions.swift
+++ SocailShare/Classes/Extensions.swift
@@ -49,7 +49,7 @@ extension UIView {
 extension UIDevice {
     
     func isiPhoneX() -> Bool {
-        let isiOS11 = systemVersionLessThanOrEqualTo(version: "11")
+        let isiOS11 = systemVersionGreaterThanOrEqualTo(version: "11.0")
         let minHeight = min(UIScreen.main.nativeBounds.size.width, UIScreen.main.nativeBounds.size.height)
         let maxLength = max(UIScreen.main.nativeBounds.size.width, UIScreen.main.nativeBounds.size.height)
         return isiOS11 && isPhone() && (minHeight >= 375  && maxLength >= 812)
@@ -67,9 +67,9 @@ extension UIDevice {
         return userInterfaceIdiom == .phone
     }
     
-    func systemVersionLessThanOrEqualTo(version: String) -> Bool {
+    func systemVersionGreaterThanOrEqualTo(version: String) -> Bool {
         let options: NSString.CompareOptions = [.numeric]
         let value = systemVersion.compare(version, options: options, range: nil, locale: nil)
-        return value != ComparisonResult.orderedDescending
+        return value != ComparisonResult.orderedAscending
     }
 }
